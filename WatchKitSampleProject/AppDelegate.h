0c455176438c7e7a93acc23d5dbe95ac7498decf
Suhail Rashid Bhat <suhailrashid@live.com>
2015-04-16 10:52:15 +0530

    Initial Commit

---
+++ WatchKitSampleProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WatchKitSampleProject
+//
+//  Created by Suhail Bhat on 16/04/15.
+//  Copyright (c) 2015 Suhail Bhat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WatchKitSampleProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WatchKitSampleProject
+//
+//  Created by Suhail Bhat on 16/04/15.
+//  Copyright (c) 2015 Suhail Bhat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
